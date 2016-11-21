f574d310de197fcca26d4f3fb14bfc8ec341b529
Ignacio Toledo <ignacio@ign.uy>
2016-11-20 23:51:22 -0300

    Added about

--- js/app.js
+++ js/app.js
@@ -1,32 +1,38 @@
+/* global localStorage */
+
 var PASS = {}
-if(localStorage.getItem('length')) {
+if (localStorage.getItem('length')) {
   PASS.length = localStorage.getItem('length')
-}
-else{
+} else {
   PASS.length = 32
 }
 
 document.title = generatePassword()
 
-var range = document.getElementById("range")
+var range = document.getElementById('range')
 range.value = PASS.length
 document.getElementById('length').textContent = PASS.length
 
-function generatePassword() {
+range.oninput = function (event) {
+  document.getElementById('length').textContent = event.target.value
+  PASS.length = event.target.value
+  localStorage.setItem('length', PASS.length)
+}
+
+function generatePassword () {
   var length = PASS.length
-  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-  var retVal = ""
+  var charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  var retVal = ''
 
   for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n))
+    retVal += charset.charAt(Math.floor(Math.random() * n))
   }
 
   return retVal
 }
 
-
-var btn = document.getElementById("btn-generate")
-btn.onclick = function(){
+var btn = document.getElementById('btn-generate')
+btn.onclick = function () {
   var pass = generatePassword()
   var input = document.getElementById('input')
   input.value = pass
@@ -34,15 +40,13 @@ btn.onclick = function(){
   input.select()
 }
 
-var btn = document.getElementById("btn-options")
-btn.onclick = function(){
+var btnOptions = document.getElementById('btn-options')
+btnOptions.onclick = function () {
   var panel = document.getElementById('panel-options')
   panel.classList.toggle('active')
 }
 
-var range = document.getElementById("range")
-range.oninput = function(event){
-  document.getElementById('length').textContent = event.target.value
-  PASS.length = event.target.value
-  localStorage.setItem('length', PASS.length)
+var btnModal = document.getElementById('js-btn-modal')
+btnModal.onclick = function () {
+  document.getElementById('body').classList.toggle('modal--active')
 }
